7b1c2870d3550d3144e2846a85c25d9ed3dd9c55
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-07-20 14:17:13 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,6 +5,6 @@ then
   PORT=5005
 fi
 
-#rasa run --model models --enable-api --cors "*" --debug -p $PORT
+rasa run --model models --credentials credentials.yml  --enable-api --cors "*" --debug -p $PORT
 
-rasa run  --model models --enable-api --port $PORT
+#rasa run  --model models --enable-api --port $PORT
